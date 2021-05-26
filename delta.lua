718e8cec5f30af9453f69a605e51c0447b7c373a
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-05-26 17:59:49 -0400

    feat: initial commit, first working version of delta

---
+++ delta.lua
@@ -0,0 +1,40 @@
+local ansikit = require 'ansikit'
+local lunacolors = require 'lunacolors'
+
+function dirty()
+	local res = io.popen 'git status --porcelain | wc -l'
+	local dirt = res:read():gsub('\n', '')
+	res:close()
+
+	return (dirt ~= '0' and '*' or '')
+end
+
+function isgitrepo()
+	local code = os.execute 'git rev-parse --git-dir > /dev/null 2>&1'
+	return code == 0
+end
+
+function branch()
+	local res = io.popen 'git rev-parse --abbrev-ref HEAD 2> /dev/null'
+	local gitbranch = res:read()
+	res:close()
+
+	return gitbranch
+end
+
+local delta = {}
+
+function delta.prompt(exitcode)
+	local fail = exitcode ~= 0
+	local promptstr = '{blue}%d'
+
+	if isgitrepo() then
+		promptstr = promptstr .. ' ' .. ansikit.getCSI('38;5;242')
+		.. branch() .. '' .. dirty()
+	end
+	promptstr = promptstr .. '\n' .. (fail and '{red}' or '{green}') .. '∆ '
+
+	return lunacolors.format(promptstr)
+end
+
+return delta
